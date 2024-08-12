e28f8e5e268b71818fdaafaa8da6a2234164acf8
TienDatzd99 <datletien2004@gmail.com>
2024-08-12 09:50:09 +0700

    Mô tả ngắn gọn về những gì đã thay đổi

--- src/services/baseService.js
+++ src/services/baseService.js
@@ -2,37 +2,40 @@ import axios from "axios"
 import { DOMAIN_CYBERBUG, TOKEN } from "../util/constants/settingSystem"
 
 export class baseService {
-    put = (url, id, model) => {
+
+    //put json về phía backend
+    put = (url,model) => {
+        return  axios({
+            url:`${DOMAIN_CYBERBUG}/${url}`,
+            method:'PUT',
+            data:model,
+            headers: {'Authorization': 'Bearer ' + localStorage.getItem(TOKEN)} //JWT
+        }) 
+    }
+
+    post = (url,model) => {
         return axios({
-            url: `${DOMAIN_CYBERBUG}/${url}`,
-            method: 'PUT',
-            data: model,
-            headers: { 'Authorization': 'Bearer ' + localStorage.getItem(TOKEN) }
+            url:`${DOMAIN_CYBERBUG}/${url}`,
+            method:'POST',
+            data:model,
+            headers: {'Authorization': 'Bearer ' + localStorage.getItem(TOKEN)} //JWT
+        }) 
+    }
+
+
+    get = (url) => {
+        return axios({
+            url:`${DOMAIN_CYBERBUG}/${url}`,
+            method:'GET',
+            headers: {'Authorization': 'Bearer ' + localStorage.getItem(TOKEN)} //token yêu cầu từ backend chứng minh user đã đăng nhập rồi
         })
     }
 
-    post = (url, id, model) => {
+    delete = (url) => {
         return axios({
-            url: `${DOMAIN_CYBERBUG}/${url}`,
-            method: 'POST',
-            data: model,
-            headers: { 'Authorization': 'Bearer ' + localStorage.getItem(TOKEN) }
-        })
-    }
-    get = (url, id, model) => {
-        return axios({
-            url: `${DOMAIN_CYBERBUG}/${url}`,
-            method: 'GET',
-            data: model,
-            headers: { 'Authorization': 'Bearer ' + localStorage.getItem(TOKEN) }
-        })
-    }
-    delete = (url, id, model) => {
-        return axios({
-            url: `${DOMAIN_CYBERBUG}/${url}`,
-            method: 'DELETE',
-            data: model,
-            headers: { 'Authorization': 'Bearer ' + localStorage.getItem(TOKEN) }
+            url:`${DOMAIN_CYBERBUG}/${url}`,
+            method:'DELETE',
+            headers: {'Authorization': 'Bearer ' + localStorage.getItem(TOKEN)} //token yêu cầu từ backend chứng minh user đã đăng nhập rồi
         })
     }
 }
